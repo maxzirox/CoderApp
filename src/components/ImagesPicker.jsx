f9bc0b269d62ec3114634307807258cb79420007
Maxi Guzman <32882558+maxzirox@users.noreply.github.com>
2023-05-14 11:43:54 -0400

    best logic in actions & reducer
    se agrega mejor lógica en  los reducer y actions, se añade cambio en tiempo real de imagen de perfil, se añade lógica para cambio de información personal, pero no funcionamiento.

--- src/components/ImagesPicker.jsx
+++ src/components/ImagesPicker.jsx
@@ -3,56 +3,59 @@ import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { styles } from '../themes/appTheme';
 import { useDispatch, useSelector } from 'react-redux';
-import { getImage } from '../store/actions/auth.action';
 import { Text } from 'react-native-paper';
+import { getImage } from '../store/actions/user.action';
 
 export const ImagesPicker = () => {
-  const userID = useSelector( state => state.auth.userId)
+  const userID = useSelector(state => state.user.userId)
+  const logId = useSelector(state => state.auth.userId)
+  const userData = useSelector( state => state.user.data)
+  const userImage = userData.map(item => item.imagen)
   const [image, setImage] = useState(null);
   const dispatch = useDispatch();
 
   
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [3, 3],
       quality: 1,
-      base64: true,
+      base64: false,
     })
-    //console.log(result);
     if (!result.canceled) {
-      //const data = new FormData();
       let uriParts = result.assets[0].uri.split('.')
       let fileType = uriParts[uriParts.length - 1]
       let date = new Date();
-      //data.append('images', {
-      //  name: `photo.${fileType}`,
-      //  type: `image/${fileType}`,
-      //  uri: result.assets[0].uri
-      //})
       setImage({
         name: `photo${date}.${fileType}`,
         type: `image/${fileType}`,
         uri: result.assets[0].uri
       });
-      console.log('uriparts: ', uriParts)
-      console.log('filetype: ', fileType)
-      console.log('data desde imagepicker: ', image)
-      console.log('userID: ', userID)
     }
   };
 
+  const onHandlerPress = () =>{
+    //console.log('imagen desde press: ', image);
+    dispatch(getImage(image.uri, userID, image.name, logId))
+    if(image !== null){setImage(null);}
+    
+  }
+
   return (
-    <View >
-      <Button type='outlined' style={styles.buttonMenu}title="Cambiar Imagen" onPress={pickImage} />
-      {image &&
+    <View>
+      {!image ?
+      <View >
+        <Image source={{ uri: `${userImage}` }} style={{ width: 100, height: 100, borderRadius: 30, alignSelf: 'center' }} />
+          <Text style={{ alignSelf: 'center', marginVertical: 10 }}>{userData.map(item => item.email)}</Text>
+        <Button type='outlined' style={styles.buttonMenu}title="Cambiar Imagen" onPress={pickImage} />
+      </View>  
+      :
       <View>
-      <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
-      <Text>{image.name}</Text>
-      <Button style={styles.buttonMenu}title="Guardar" onPress={()=>{ dispatch(getImage(image.uri, userID[0], image.name))}} />
+        <Image source={{ uri: image.uri }} style={{ width: 100, height: 100, borderRadius: 30, alignSelf: 'center' }} />
+          <Text style={{fontSize: 10, color: 'red', alignSelf: 'center'}}>{image.name}</Text>
+        <Button style={styles.buttonMenu}title="Guardar" onPress={()=> onHandlerPress()} />
       </View> }
     </View>
   );
---
+++ src/store/actions/user.action.jsx
@@ -0,0 +1,125 @@
+import {  collection, doc, arrayUnion, getDocs, query, updateDoc, where } from "firebase/firestore";
+import dataBase from "../../utils/firebase";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+
+export const GETINFO = 'GET_INFO';
+export const GET_IMAGE = 'GET_IMAGE';
+export const ADD_PLACE = 'ADD_PLACE';
+export const EDIT_INFO = 'EDIT_INFO'
+
+export const addPlace = ( newAddress, id, userId ) => {
+    return async dispatch => {
+        
+            const userDocRef = doc(dataBase, 'usuarios', id)
+            try{
+              //con arayUnion agregamos un nuevo elemento a las direcciones
+              await updateDoc(userDocRef, { address: arrayUnion(newAddress)})
+              .then(alert(`Nueva direccion registrada con exito`))
+              const userQuery = query(collection(dataBase, 'usuarios'), where('userId', '==', userId))
+              const querySnapshot = await getDocs(userQuery)
+              const userData = querySnapshot.docs.map(item => item.data());
+              dispatch({
+                type: ADD_PLACE,
+                data: userData,
+                confirm: true,
+              })
+            } catch (err) {
+              alert(err)
+            }    
+          }
+    }
+
+
+export const getInfo = (userId) =>{
+
+    return async dispatch => {
+
+        const userQuery = query(collection(dataBase, 'usuarios'), where('userId', '==', userId))
+        const querySnapshot = await getDocs(userQuery)
+        const userID = querySnapshot.docs.map(item => item.id)
+        const userData = querySnapshot.docs.map(item => item.data());
+        console.log('id coleccion user: ', userID[0]);
+        dispatch({
+            type: GETINFO,
+            //token: data.idToken,
+            userId: userID[0],
+            data: userData,
+        });
+
+    }
+
+}
+
+
+export const getImage = (image, userId, name, logId) =>{
+  return async dispatch => {
+          const userDocRef = doc(dataBase, 'usuarios', userId)
+          //aqui convertimos la imagen a blob
+          const blobImage = await new Promise((resolve, reject) => {
+              const xhr = new XMLHttpRequest();
+              xhr.onload = () =>{
+                  resolve(xhr.response);
+              };
+              xhr.onerror = () => {
+                  reject(new TypeError('Network request failed'));
+              };
+              xhr.responseType = 'blob';
+              xhr.open("GET", image, true);
+              xhr.send(null);
+          })
+          try{
+              let URL = '';
+              const storage = getStorage();
+              const storageRef = ref(storage, `avatar/${name}`);
+              const metadata = {
+                  contentType: 'image/jpeg',
+                };
+              // 'file' comes from the Blob or File API
+              await uploadBytes(storageRef, blobImage, metadata).then((snapshot) => {
+              console.log('Uploaded a blob or file!');
+              });
+              URL = await getDownloadURL(storageRef).then((downloadURL) => downloadURL);
+              console.log('image desde action: ', URL)
+
+               await updateDoc(userDocRef, {imagen: URL})
+            .then(alert('imagen cambiada'))
+            
+          //const userQuery = query(collection(dataBase, 'usuarios')
+          const userQuery = query(collection(dataBase, 'usuarios'), where('userId', '==', logId))
+          const querySnapshot = await getDocs(userQuery)
+          const userData = querySnapshot.docs.map(item => item.data())
+            console.log('data desde user action: ',userData);
+            dispatch({
+              type: GET_IMAGE,
+              data: userData,
+              confirm: true
+            })
+          } catch (err) {
+            alert(err)
+          } 
+  }
+}
+
+export const editInfo = ( nombre, email, logId) => {
+  return async dispatch => {
+    const userDocRef = doc(dataBase, 'usuarios', id)
+    try{
+      
+      await updateDoc(userDocRef, { 
+        name: nombre,
+        email: email, 
+      })
+      .then(alert(`Nueva direccion registrada con exito`))
+      const userQuery = query(collection(dataBase, 'usuarios'), where('userId', '==', logId))
+      const querySnapshot = await getDocs(userQuery)
+      const userData = querySnapshot.docs.map(item => item.data());
+      dispatch({
+        type: ADD_PLACE,
+        data: userData,
+        confirm: true,
+      })
+    } catch (err) {
+      alert(err)
+    }    
+  }
+}
