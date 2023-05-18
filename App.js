7160fd14d7d6d517c312df2cf9f2867abb0b48e1
Maxi Guzman <32882558+maxzirox@users.noreply.github.com>
2023-05-18 09:44:06 -0400

    add statusbar

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import store from './src/store';
 import { Provider } from 'react-redux';
 import { NavigatorContainer } from './src/navigator/NavigatorContainer';
+import { StatusBar } from 'react-native';
+
 
 export default function App() {
 
   return (
     <Provider store={store} >
+        <StatusBar
+        animated={true}
+        backgroundColor="#2B124C"
+      />
       <NavigatorContainer/>
     </Provider>
   );
