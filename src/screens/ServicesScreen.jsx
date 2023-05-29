1507cc9855e59831fa0710479fb3632f7f26537a
Maxi Guzman <32882558+maxzirox@users.noreply.github.com>
2023-05-29 11:46:08 -0400

    Update ServicesScreen.jsx

--- src/screens/ServicesScreen.jsx
+++ src/screens/ServicesScreen.jsx
@@ -1,38 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
-
-import * as Location from 'expo-location';
-import { styles } from '../themes/appTheme';
+import React from 'react'
+import { Text } from 'react-native-paper'
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useState } from 'react';
+import { Button, FlatList, TouchableOpacity, View } from 'react-native';
 
 export const ServicesScreen = () => {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+  
+    const onChange = (event, selectedDate) => {
+      const currentDate = selectedDate;
+      //setShow(false);
+      setDate(currentDate);
+    };
+  
+    const showMode = (currentMode) => {
+      if (Platform.OS === 'android') {
+        setShow(false);
+        // for iOS, add a button that closes the picker
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-  return (
-    <View >
-      <Text>{text}</Text>
-    </View>
-  );
-}
-
+      setMode(currentMode);
+    };
+  
+    const days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    days.map((day) => console.log(`Dia ${day} desde componente`))
+    return (
+      <View>
+        <View>
+          <FlatList 
+            data={days}
+            renderItem={(day) => ( 
+            <View>
+              <TouchableOpacity style={{backgroundColor: 'blue', width: 100, height: 50, borderRadius: 20, margin: 5}}>
+                <Text style={{color: 'aliceblue', fontSize: 19, textAlign: 'center'}}>{day.item}</Text>
+              </TouchableOpacity>
+            </View>
+            )}
+            horizontal={true}
+          />
+        </View>
+        <View>
+          <FlatList 
+            data={days}
+            renderItem={(day) => ( 
+            <View>
+              <TouchableOpacity style={{backgroundColor: 'blue', width: 250, height: 50, borderRadius: 20, margin: 5, alignSelf: 'center'}}>
+                <Text style={{color: 'aliceblue', fontSize: 19, textAlign: 'center'}}>{day.item}</Text>
+              </TouchableOpacity>
+            </View>
+            )}
+            
+          />
+        </View>
+      </View>
+    );
+  };
